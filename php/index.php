268ab2003ad969eb8d4d583b1b3e7342e8de1aaa
YuriLoro <yuri.fernandes.senai404@gmail.com>
2024-08-02 10:37:07 -0300

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<?php
+$nome = 'Yuri';
+$escola = 'SESI';
+$cidade = 'cerquilho';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas';
+$media = 9.5;
+
+echo 'Olá bem vindo ao SESI-SP <br>';
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+echo "Olá $nome, seja bem vindo ao $escola $CE de $cidade, sua nota no curso de $curso é: $media."
+
+?>
