9d178d50fd072dd37733e3bac760a7421826bf5b
YuriLoro <yuri.fernandes.senai404@gmail.com>
2024-08-16 09:40:20 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+// definicao das variaveis 
+$nome = 'Enzo ';
+$escola = 'SESI Cerquilho <br>';
+$cidade = 'Cerquilho <br>';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas <br>';
+$media = 9.5;
+
+//chamando as variaveis 
+echo $nome;
+echo $escola;
+echo $CE;
+echo $curso;
+echo $media;
+
+//exemplo de introduçao das variaveis
+echo "Ola $nome, Bem vindo ao SESI $CE de $cidade <br>";
+echo 'Ola mundo <br> '; 
+echo 'Bem Vindo ao Sesi/Senai <br>';  
+
+
+?>
